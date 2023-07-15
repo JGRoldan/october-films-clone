3a20338bc89cb481102ba1cde923c138b9852348
JGRDeveloper <jgroldan.dev@gmail.com>
2023-07-15 12:31:58 -0300

    Modified directions on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,10 +11,20 @@ const Contact = () => {
             <main className='w-full h-screen'>
                 <SecondayNavBar/>
                 <ContactInfo city="London">
-                    Datos del lugar
+                    <pre>
+                    Benwell Studios
+                    11/13 Benwell Road
+                    London N7 7BL
+                    info@octoberfilms.co.uk
+                    </pre>
                 </ContactInfo>
                 <ContactInfo city="Los Angeles">
-                    Datos del lugar
+                    <pre>
+                    10880 Wilshire Boulevard
+                    Suite 1101
+                    Los Angeles CA 90024
+                    info@octoberfilms.co.uk
+                    </pre>
                 </ContactInfo>
                 <Footer />
             </main>  
